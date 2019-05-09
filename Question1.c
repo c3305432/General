3610aff673ed583f7fc1a2bac235956bb7dab2b0
Ryan Mistelbauer <c3305432@uon.edu.au>
2019-05-09 04:36:57 +0000

    iyl

--- Question1.c
+++ Question1.c
@@ -1,6 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
 
 int main() {
 
-return 0;
+    float image[5][5][3];
+    float mean;
+    int x, y;
+    // Don’t change this srand() line!
+    srand(0);
+    for(y = 0; y < 2; y++) {
+        for(x = 0; x < 5; x++) {
+            image[x][y][0] = (float)rand()/(float)INT_MAX;
+            image[x][y][1] = (float)rand()/(float)INT_MAX;
+            image[x][y][2] = (float)rand()/(float)INT_MAX;
+            
+            mean = ((image[x][y][0]) + (image[x][y][1])+(image[x][y][2]))/3.0;
+            
+            image[x][y][0] = mean;
+            image[x][y][1] = mean;
+            image[x][y][2] = mean;
+            printf("%d %d: %f %f %f\n", x, y, image[x][y][0], image[x][y][1], image[x][y][2]);
+        }
+    }
+    return 0;
 }
